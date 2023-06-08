5ac7a6768d4fc601c16b2b1e43ca767159c1a65b
Nihal Ahmad <nahmad.bee19seecs@seecs.edu.pk>
2023-06-08 13:21:26 +0500

    Added a bit of UI with JS

--- rps.js
+++ rps.js
@@ -1,6 +1,7 @@
-let draws=0;
-let playerWins=0;
-let computerWins=0;
+let draws = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 function getComputerChoice(){
 const choices=["Rock", "Paper", "Scissor"];
 let index=Math.floor(Math.random() * 3);
@@ -11,34 +12,42 @@ return finalChoice;
 function playRound(playerSelection, computerSelection){
 if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
 draws +=1;
-return "Draw! You both chose the same."
+let statement = "Draw! You both chose the same.";
+return statement;
 }
 else if(playerSelection.toLowerCase()=="rock" && computerSelection.toLowerCase()=="paper"){
 computerWins +=1;
-return "Computer Wins! Paper beats Rock"
+let statement = "Computer Wins! Paper beats Rock";
+return statement;
 }
 else if(playerSelection.toLowerCase()=="rock" && computerSelection.toLowerCase()=="scissor"){
 playerWins +=1;
-return "You Win! Rock beats scissor"
+let statement = "You Win! Rock beats scissor";
+return statement;
 }
 else if(playerSelection.toLowerCase()=="paper" && computerSelection.toLowerCase()=="rock"){
 playerWins +=1;
-return "You Win! Paper beats Rock"
+let statement = "You Win! Paper beats Rock";
+return statement;
 }
 else if(playerSelection.toLowerCase()=="paper" && computerSelection.toLowerCase()=="scissor"){
 computerWins +=1;   
-return "Computer Wins! Scissors cut paper"
+let statement = "Computer Wins! Scissor cuts paper";
+return statement;
 }
 else if(playerSelection.toLowerCase()=="scissor" && computerSelection.toLowerCase()=="rock"){
 computerWins +=1;
-return "Computer Wins! Rock beats scissors"
+let statement = "Computer Wins! Rock beats scissors";
+return statement;
 }
 else if(playerSelection.toLowerCase()=="scissor" && computerSelection.toLowerCase()=="paper"){
 playerWins +=1;
-return "You Win! Scissors cut paper"
+let statement = "You Win! Scissors cut paper";
+return statement;
 }
 }
-function game(){
+
+/*function game(){
 for(let i=0;i<5;i++){
     let playerSelection = prompt("Rock, Paper or Scissor", "Rock");
     const computerSelection = getComputerChoice();
@@ -48,4 +57,66 @@ console.log("Total Draws: " + draws);
 console.log("Total Player Wins: " + playerWins);
 console.log("Total Computer Wins: " + computerWins);
 }
-game();
+game();*/
+function game_rock(){
+    let playerSelection = "Rock";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    div.textContent = `Total Player Wins: ${playerWins} and Total CPU Wins ${computerWins}`;
+    if(playerWins == 5){
+        div.appendChild(winner);
+    }
+    else if(computerWins == 5){
+        div.appendChild(winnerComputer);
+    }
+}
+
+function game_paper(){
+    let playerSelection = "Paper";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    div.textContent = `Total Player Wins: ${playerWins} and Total CPU Wins ${computerWins}`;
+    if(playerWins == 5){
+        div.appendChild(winner);
+    }
+    else if(computerWins == 5){
+        div.appendChild(winnerComputer);
+    }
+}
+
+function game_scissor(){
+    let playerSelection = "Scissor";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    div.textContent = `Total Player Wins: ${playerWins} and Total CPU Wins ${computerWins}`;
+    if(playerWins == 5){
+        div.appendChild(winner);
+    }
+    else if(computerWins == 5){
+        div.appendChild(winnerComputer);
+    }
+}
+
+const btn_rock = document.querySelector('.rock');
+btn_rock.addEventListener('click',game_rock);
+
+const btn_paper = document.querySelector('.paper');
+btn_paper.addEventListener('click',game_paper);
+
+const btn_scissor = document.querySelector('.scissor');
+btn_scissor.addEventListener('click',game_scissor);
+
+const div = document.createElement('div');
+div.textContent = `Total Player Wins: ${playerWins} and Total CPU Wins ${computerWins}`;
+div.setAttribute('id','result');
+div.style.cssText = 'color: blue; background: white;';
+const textDiv = document.querySelector('.text');
+textDiv.appendChild(div);
+
+const winner = document.createElement('h1');
+winner.textContent = "The Player Wins!"
+winner.style.cssText = 'color: blue; background: white;';
+
+const winnerComputer = document.createElement('h1');
+winnerComputer.textContent = "The Computer Wins!"
+winnerComputer.style.cssText = 'color: red; background: white;';
